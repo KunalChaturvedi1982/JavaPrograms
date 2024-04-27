6fd9256024e2b4fa5387c37db0751cb0aec348bb
Kunal <c.kunal048@gmail.com>
2024-04-27 17:40:57 +0530

    Added Factorial program

---
+++ JavaPrograms/src/javaProgramsPackage/Factorial.java
@@ -0,0 +1,21 @@
+package javaProgramsPackage;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		int i, fact=1, num;
+		System.out.println("Enter any number:");
+		Scanner sc= new Scanner(System.in);
+		num = sc.nextInt();
+		
+		for(i=1;i<=num;i++)
+		{
+			fact = fact * i;
+		}
+		System.out.println("Factorial of " + num + " is: " + fact);
+
+	}
+
+}
